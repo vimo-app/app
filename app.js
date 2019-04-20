a127f8c93116fb90b7abacd309c8621e8f73ecf9
Raul Contreras <raulc98@gmail.com>
2019-04-20 13:11:15 +0200

    Fixed app.js on node-sass-middleware to compile imported scss files

--- app.js
+++ app.js
@@ -39,6 +39,8 @@ app.use(cookieParser());
 app.use(require('node-sass-middleware')({
   src:  path.join(__dirname, 'public'),
   dest: path.join(__dirname, 'public'),
+  debug:true,
+  force:true,
   sourceMap: true
 }));
 
