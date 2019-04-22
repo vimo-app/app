4d8b9e22dafae17d863846e78d9401cb0d15483f
Raul Contreras <raulc98@gmail.com>
2019-04-22 21:59:43 +0200

    Created route to get the first five recent photos

--- routes/flickr.js
+++ routes/flickr.js
@@ -3,35 +3,33 @@ const router = express.Router();
 const Flickr = require('flickr-sdk');
 const flickr = new Flickr(process.env.FLICKR_ID);
 
-router.get('/search/:query', (req, res, next) => {
-  flickr.photos.search({
-    text: req.params.query,
-    page: Math.ceil(Math.random()*10)
+router.get('/home/photos', (req, res, next) => {
+  flickr.photos.getRecent({
+    per_page: 5
   })
-  .then(photos => {
-    res.status(200).json({status: 200, response: JSON.parse(photos.text).photos.photo});
-  })
-  .catch(err => {
-    console.error(err);
-    res.status(500).json({error:err});
-  });
-});
+  .then(photos => Promise.resolve(JSON.parse(photos.text).photos.photo))
+  .then(recentPhotos => Promise.resolve(recentPhotos.map(obj => {
+      let getInfo = flickr.photos.getInfo({
+        photo_id: obj.id
+      });
+      let getImage = flickr.photos.getSizes({
+        photo_id: obj.id
+      });
 
-router.get('/photos/:id', (req, res, next) => {
-  flickr.photos.getInfo({
-    photo_id: req.params.id
+      return Promise.all([getInfo, getImage])
+        .then(promises => ({
+          username: JSON.parse(promises[0].text).photo.owner.username,
+          realname: JSON.parse(promises[0].text).photo.owner.realname,
+          image: JSON.parse(promises[1].text).sizes.size.filter(obj => obj.label === 'Small 320')[0].source,
+          url: JSON.parse(promises[1].text).sizes.size.filter(obj => obj.label === 'Small 320')[0].url
+        }))
+        .catch(err => console.error(err));
+    })))
+  .then(val => {
+    Promise.all(val)
+    .then(response => res.status(200).json({status: 200, response}))
+    .catch(err => console.log(err));
   })
-  .then(photo => res.status(200).json({status: 200, response: JSON.parse(photo.text)}))
-  .catch(err => {
-    console.error(err);
-    res.status(500).json({error:err});
-  });
-});
-router.get('/photos/sizes/:id', (req, res, next) => {
-  flickr.photos.getSizes({
-    photo_id: req.params.id
-  })
-  .then(photo => res.status(200).json({status: 200, response: JSON.parse(photo.text).sizes.size}))
   .catch(err => {
     console.error(err);
     res.status(500).json({error:err});
