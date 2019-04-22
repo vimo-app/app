e8354391edf2995a39488251cce5a2460ec035b0
Raul Contreras <raulc98@gmail.com>
2019-04-22 22:18:30 +0200

    The home images loads dinamically

--- public/javascripts/image-grid.js
+++ public/javascripts/image-grid.js
@@ -1,6 +1,20 @@
 window.onload = function(){
+  let imageGrid = document.querySelector('#image-grid');
+  let anchor,img,div;
   axios.get(`${window.location.origin}/flickr/home/photos`)
     .then(response => {
       let images = response.data.response;
+      images.forEach(image => {
+        anchor = document.createElement('a');
+        img = document.createElement('img');
+        div = document.createElement('div');
+        anchor.setAttribute('href', image.url);
+        img.setAttribute('src', image.image);
+        div.setAttribute('class', 'normal-content__image-grid-author');
+        div.innerHTML = `<span>${image.realname !== '' ? image.realname : image.username}</span>`;
+        anchor.appendChild(img);
+        anchor.appendChild(div);
+        imageGrid.appendChild(anchor);
+      });
     });
 };
