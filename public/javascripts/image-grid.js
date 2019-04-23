65b81626b65fefb89e1c4aa53aa46bdbb7d07931
Raúl Contreras Rubio <raulc98@gmail.com>
2019-04-23 09:34:24 +0200

    Flickr api home call (#10)
    * Added flickr-sdk package
    
    * Created API routes
    
    * Created route to get the first five recent photos
    
    * Now the frontend gets the backend info
    
    * The home images loads dinamically
    
    * Implemented home functionality
    
    * Removed some junk code
    
    * The home images are loaded at least 5 times before manual loading required

--- public/javascripts/image-grid.js
+++ public/javascripts/image-grid.js
@@ -1,3 +1,30 @@
 (function(){
-
-}());
+  let imageGrid = document.querySelector('#image-grid');
+  let spinner = document.querySelector('#spinner');
+  let anchor,img,div;
+  let pageCounter = 1;
+  
+  function loadImages(page = 1){
+    axios.get(`${window.location.origin}/flickr/home/photos/${page}`)
+    .then(response => {
+      let images = response.data.response;
+      images.forEach(image => {
+        anchor = document.createElement('a');
+        img = document.createElement('img');
+        div = document.createElement('div');
+        anchor.setAttribute('href', image.url);
+        anchor.setAttribute('target', '_blank');
+        img.setAttribute('src', image.image);
+        div.setAttribute('class', 'normal-content__image-grid-author');
+        div.innerHTML = `<span>${image.realname !== '' ? image.realname : image.username}</span>`;
+        anchor.appendChild(img);
+        anchor.appendChild(div);
+        imageGrid.appendChild(anchor);
+      });
+      spinner.onclick = loadImages.bind(null, pageCounter++);
+      if(pageCounter < 5){ loadImages(pageCounter);}
+    });
+  }
+  loadImages(pageCounter)
+  
+})();
---
+++ routes/flickr.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const router = express.Router();
+const Flickr = require('flickr-sdk');
+const flickr = new Flickr(process.env.FLICKR_ID);
+
+router.get('/home/photos/:page?', (req, res, next) => {
+  flickr.photos.search({
+    page: req.params.page !== "" ? +req.params.page : 1,
+    per_page: 5,
+    text: 'landscape'
+  })
+  .then(photos => Promise.resolve(JSON.parse(photos.text).photos.photo))
+  .then(recentPhotos => Promise.resolve(recentPhotos.map(obj => {
+      let getInfo = flickr.photos.getInfo({
+        photo_id: obj.id
+      });
+      let getImage = flickr.photos.getSizes({
+        photo_id: obj.id
+      });
+
+      return Promise.all([getInfo, getImage])
+        .then(promises => ({
+          username: JSON.parse(promises[0].text).photo.owner.username,
+          realname: JSON.parse(promises[0].text).photo.owner.realname,
+          image: JSON.parse(promises[1].text).sizes.size.filter(obj => obj.label === 'Small 320')[0].source,
+          url: JSON.parse(promises[1].text).sizes.size.filter(obj => obj.label === 'Small 320')[0].url
+        }))
+        .catch(err => console.error(err));
+    })))
+  .then(val => {
+    Promise.all(val)
+    .then(response => res.status(200).json({status: 200, response}))
+    .catch(err => console.log(err));
+  })
+  .catch(err => {
+    console.error(err);
+    res.status(500).json({error:err});
+  });
+});
+
+module.exports = router;
